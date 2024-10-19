3506529a9f583c443c9910f8fcc773a09c530816
Leonid Dorochko <mrbelarusman@gmail.com>
2024-10-20 00:51:40 +0200

    Change expressions in Regex.Union from List<Regex> to Set<Regex>

--- app/src/main/kotlin/org/exeval/automata/interfaces/Regex.kt
+++ app/src/main/kotlin/org/exeval/automata/interfaces/Regex.kt
@@ -2,7 +2,7 @@ package org.exeval.automata.interfaces
 
 sealed interface Regex {
     data class Atom(val char: Char): Regex
-    data class Union(val expressions: List<Regex>): Regex
+    data class Union(val expressions: Set<Regex>): Regex
     data class Concat(val expressions: List<Regex>): Regex
     data class Star(val expression: Regex): Regex
 }
