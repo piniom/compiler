7237364a5e73b856c38fd2373ae64523bafa4410
Leonid Dorochko <mrbelarusman@gmail.com>
2024-10-20 00:52:18 +0200

    Add exception for RegexParser, that will be thrown if regex is wrong

--- app/src/main/kotlin/org/exeval/lexer/interfaces/RegexParser.kt
+++ app/src/main/kotlin/org/exeval/lexer/interfaces/RegexParser.kt
@@ -3,5 +3,8 @@ package org.exeval.lexer.interfaces
 import org.exeval.automata.interfaces.Regex
 
 interface RegexParser {
+    // Should throw BadRegexFormatException if pattern is invalid
     fun parse(pattern: String): Regex
 }
+
+class BadRegexFormatException(message: String) : Exception(message)
