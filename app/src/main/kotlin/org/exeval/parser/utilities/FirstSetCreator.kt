c0cbbd6f3edac5a3c5e3060d4a0adaa035317856
Mapet13 <jakubsordyl1@gmail.com>
2024-10-24 00:28:11 +0200

    Add First Set creator

---
+++ app/src/main/kotlin/org/exeval/parser/utilities/FirstSetCreator.kt
@@ -0,0 +1,41 @@
+package org.exeval.parser.utilities
+
+import org.exeval.parser.Grammar
+
+fun <S> createFirstSet(grammar: Grammar<S>, nullable: Set<S>): Map<S, List<S>> {
+    return transitiveClosure(
+        addPossibleStates(
+            crateMapWithOnlyKeySymbol(grammar), grammar, nullable
+        )
+    ).mapValues { it.value.toList() }
+}
+
+private fun <S> crateMapWithOnlyKeySymbol(grammar: Grammar<S>): Map<S, MutableList<S>> {
+    return grammar.productions.map { it.left }.associateWith { mutableListOf(it) }
+}
+
+private fun <S> addPossibleStates(
+    first: Map<S, MutableList<S>>, grammar: Grammar<S>, nullable: Set<S>
+): Map<S, MutableList<S>> {
+    first.forEach { (symbol, firstForSymbol) ->
+        grammar.productions.filter { it.left == symbol && it.right.isNotEmpty() }.forEach { productions ->
+            for (currentSymbol in productions.right) {
+                firstForSymbol.add(symbol)
+                if (!nullable.contains(currentSymbol)) break
+            }
+        }
+    }
+    return first
+}
+
+private fun <S> transitiveClosure(first: Map<S, MutableList<S>>): Map<S, MutableList<S>> {
+    repeat(first.size) {
+        first.forEach { (_, list) ->
+            list.addAll(list.fold(mutableListOf()) { newList, symbol ->
+                newList.addAll(first[symbol].orEmpty())
+                newList
+            })
+        }
+    }
+    return first
+}
