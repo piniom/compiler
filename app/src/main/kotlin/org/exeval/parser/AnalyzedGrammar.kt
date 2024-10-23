007825ea607fb5246b1b9242f5744b7e380b8984
Dominik Chmura <dominik06252001@gmail.com>
2024-10-23 09:47:35 +0200

    Add grammar to AnalyzedGrammar

--- app/src/main/kotlin/org/exeval/parser/AnalyzedGrammar.kt
+++ app/src/main/kotlin/org/exeval/parser/AnalyzedGrammar.kt
@@ -1,6 +1,9 @@
 package org.exeval.parser
 
+import org.exeval.parser.Grammar
+
 data class AnalyzedGrammar<S> (
     val nullable: Set<S>,
     val firstProduct: Map<S, List<S>>,
+    val grammar: Grammar<S>,
 )
