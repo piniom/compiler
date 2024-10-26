5754e0bfc240e496fc597041fd2d06dcf5b88e47
Leonid Dorochko <mrbelarusman@gmail.com>
2024-10-26 21:28:05 +0200

    Add parser and start adding tests

--- app/src/main/kotlin/org/exeval/parser/Parser.kt
+++ app/src/main/kotlin/org/exeval/parser/Parser.kt
@@ -1,13 +1,85 @@
 package org.exeval.parser
 
 import org.exeval.parser.interfaces.ParseTree
+import java.util.Stack
 
 class Parser<S> {
+//    private val rawParser: RawParser<S, *>
+
     constructor(grammar: AnalyzedGrammar<S>) {
         //...
+
+//        TODO("Define state, actions and goto")
+//        val startSymbol: S = grammar.grammar.startSymbol
+//        val endSymbol: S = grammar.grammar.endOfParse
+//        val startingState: State
+//        val actions: Map<Pair<Symbol, State>, Action<Symbol>>,
+//        val goto: Map<Pair<Symbol, State>, Symbol>
+//        rawParser = RawParser<Symbol, Any>(startSymbol, endSymbol, startingState, actions, goto)
     }
 
-    fun run(leaves: List<ParseTree.Leaf<S>>) {
-        //...
+    fun run(leaves: List<ParseTree.Leaf<S>>): ParseTree<S> {
+        TODO("Not implemented")
+//        return rawParser.run(leaves)
     }
 }
+
+
+class RawParser<Symbol, State>(
+    private val startSymbol: Symbol,
+    private val endSymbol: Symbol,
+    private val startState: State,
+    private val actions: Map<Pair<Symbol, State>, Action<Symbol, State>>,
+    private val goto: Map<Pair<Symbol, State>, State>
+) {
+    private val stack = Stack<Pair<ParseTree<Symbol>, State>>()
+
+    fun run(leaves: List<ParseTree.Leaf<Symbol>>): ParseTree<Symbol> {
+        stack.clear()
+        stack.push(
+            ParseTree.Leaf<Symbol>(
+                startSymbol, leaves.first().startLocation, leaves.last().endLocation
+            ) to startState
+        )
+        var leafI = 0
+
+        while (true) {
+            val state: State = stack.peek().second
+            val leaf: ParseTree.Leaf<Symbol> = leaves[leafI]
+
+            val curAction = actions[leaf.symbol to state]
+            if (curAction is Action.Reduce) {
+                val prod = curAction.production
+                val newBranch = takeProductionFromStack(prod)
+                val newState = goto[prod.left to stack.peek().second] ?: throw ParseError("Goto is incomplete")
+                stack.push(newBranch to newState)
+            } else if (curAction is Action.Shift) {
+                val newState = curAction.state
+                stack.push(leaf to newState)
+                leafI += 1
+            } else if (curAction is Action.Accept && leaf.symbol == endSymbol) {
+                return stack.peek().first
+            } else {
+                throw ParseError("Parse error at leaf %s".format(leaf))
+            }
+        }
+
+    }
+
+    private fun takeProductionFromStack(prod: Production<Symbol>): ParseTree.Branch<Symbol> {
+        val children = mutableListOf<ParseTree<Symbol>>()
+        for (i in 1..prod.right.size) children.add(stack.pop().first)
+        children.reverse()
+        val newBranch = ParseTree.Branch(prod, children, children.first().startLocation, children.last().endLocation)
+
+        return newBranch
+    }
+}
+
+sealed interface Action<Symbol, State> {
+    data class Reduce<Symbol, State>(val production: Production<Symbol>) : Action<Symbol, State>
+    data class Shift<Symbol, State>(val state: State) : Action<Symbol, State>
+    class Accept<Symbol, State> : Action<Symbol, State>
+}
+
+class ParseError(override val message: String) : Exception()
---
+++ app/src/test/kotlin/org/exeval/parser/parser/RawParserParenthesisTest.kt
@@ -0,0 +1,115 @@
+package org.exeval.parser.parser
+
+import org.exeval.input.interfaces.Location
+import org.exeval.parser.Action
+import org.exeval.parser.Grammar
+import org.exeval.parser.ParseError
+import org.exeval.parser.Parser
+import org.exeval.parser.Production
+import org.exeval.parser.RawParser
+import org.exeval.parser.interfaces.ParseTree
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+// ParenthesisSymbol - ParSym, it's symbol in the grammar that codes valid parenthesis sequences
+private enum class ParSym {
+    START, LIST, PAIR, OPEN, CLOSE, END
+}
+
+private val grammar = Grammar(
+    startSymbol = ParSym.START, endOfParse = ParSym.END, productions = listOf(
+        Production(ParSym.START, listOf(ParSym.LIST)),
+        Production(ParSym.LIST, listOf(ParSym.LIST, ParSym.PAIR)),
+        Production(ParSym.LIST, listOf(ParSym.PAIR)),
+        Production(ParSym.PAIR, listOf(ParSym.OPEN, ParSym.LIST, ParSym.CLOSE)),
+        Production(ParSym.PAIR, listOf(ParSym.OPEN, ParSym.CLOSE)),
+    )
+)
+
+class RawParserParenthesisTest {
+    private val actions: Map<Pair<ParSym, Int>, Action<ParSym, Int>> = mapOf(
+        (ParSym.END to 1) to Action.Accept(),
+        (ParSym.END to 2) to Action.Reduce(grammar.productions[2]),
+        (ParSym.END to 4) to Action.Reduce(grammar.productions[1]),
+        (ParSym.END to 8) to Action.Reduce(grammar.productions[4]),
+        (ParSym.END to 10) to Action.Reduce(grammar.productions[3]),
+
+        (ParSym.OPEN to 0) to Action.Shift(3),
+        (ParSym.OPEN to 1) to Action.Shift(3),
+        (ParSym.OPEN to 2) to Action.Reduce(grammar.productions[2]),
+        (ParSym.OPEN to 3) to Action.Shift(7),
+        (ParSym.OPEN to 4) to Action.Reduce(grammar.productions[1]),
+        (ParSym.OPEN to 5) to Action.Shift(7),
+        (ParSym.OPEN to 6) to Action.Reduce(grammar.productions[2]),
+        (ParSym.OPEN to 7) to Action.Shift(7),
+        (ParSym.OPEN to 8) to Action.Reduce(grammar.productions[4]),
+        (ParSym.OPEN to 9) to Action.Reduce(grammar.productions[1]),
+        (ParSym.OPEN to 10) to Action.Reduce(grammar.productions[3]),
+        (ParSym.OPEN to 11) to Action.Shift(7),
+        (ParSym.OPEN to 12) to Action.Reduce(grammar.productions[4]),
+        (ParSym.OPEN to 13) to Action.Reduce(grammar.productions[3]),
+
+        (ParSym.CLOSE to 3) to Action.Shift(8),
+        (ParSym.CLOSE to 5) to Action.Shift(10),
+        (ParSym.CLOSE to 6) to Action.Reduce(grammar.productions[2]),
+        (ParSym.CLOSE to 7) to Action.Shift(12),
+        (ParSym.CLOSE to 9) to Action.Reduce(grammar.productions[1]),
+        (ParSym.CLOSE to 11) to Action.Shift(13),
+        (ParSym.CLOSE to 12) to Action.Reduce(grammar.productions[4]),
+        (ParSym.CLOSE to 13) to Action.Reduce(grammar.productions[3]),
+    )
+    private val goto: Map<Pair<ParSym, Int>, Int> = mapOf(
+        (ParSym.LIST to 0) to 1,
+        (ParSym.LIST to 3) to 5,
+        (ParSym.LIST to 7) to 11,
+        (ParSym.PAIR to 0) to 2,
+        (ParSym.PAIR to 1) to 4,
+        (ParSym.PAIR to 3) to 6,
+        (ParSym.PAIR to 5) to 9,
+        (ParSym.PAIR to 7) to 6,
+        (ParSym.PAIR to 11) to 9,
+    )
+    private val rawParser = RawParser<ParSym, Int>(grammar.startSymbol, grammar.endOfParse, 0, actions, goto)
+
+    @Test
+    fun `() works`() {
+        val leaves = stringToLeaves("()")
+
+        val actual = rawParser.run(leaves)
+    }
+
+//    @Test
+//    fun `(())() works`() {
+//
+//    }
+//
+    @Test
+    fun `()) throws error`() {
+        val leaves = stringToLeaves("())")
+
+        assertThrows<ParseError> { rawParser.run(leaves) }
+    }
+
+    private fun stringToLeaves(str: String): List<ParseTree.Leaf<ParSym>> {
+        val body = str.filter { c -> c == '(' || c == ')' }.mapIndexed { i, char ->
+            val loc = Loc(i)
+            val res: ParseTree.Leaf<ParSym> = when (char) {
+                '(' -> ParseTree.Leaf(ParSym.OPEN, loc, loc)
+                ')' -> ParseTree.Leaf(ParSym.CLOSE, loc, loc)
+                else -> throw Exception("Filter does not work...")
+            }
+            res
+        }
+        val endLoc = Loc(body.size)
+        val tail = listOf(ParseTree.Leaf(ParSym.END, endLoc, endLoc))
+
+        return body + tail
+    }
+}
+
+
+private data class Loc(
+    override val idx: Int
+) : Location {
+    override val line = 0
+}
