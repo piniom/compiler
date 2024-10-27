03e298f4c84a618e0218ce746113e0103ea2c619
KacKoz <42511379+KacKoz@users.noreply.github.com>
2024-10-27 20:32:24 +0100

    DFA parser (#48)
    * Add NFA to DFA parser with tests
    ---------
    
    Co-authored-by: PiJayson <pijay888@gmail.com>

--- app/src/main/kotlin/org/exeval/lexer/interfaces/DFAParser.kt
+++ app/src/main/kotlin/org/exeval/lexer/interfaces/DFAParser.kt
@@ -4,5 +4,5 @@ import org.exeval.automata.interfaces.DFA
 import org.exeval.automata.interfaces.NFA
 
 interface DFAParser<S> {
-    fun parse(nfa: NFA<S>): DFA<S>
+    fun parse(nfa: NFA<S>): DFA<*>
 }
