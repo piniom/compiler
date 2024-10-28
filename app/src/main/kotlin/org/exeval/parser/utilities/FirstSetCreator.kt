c6b6a3af5a597f67368bef82852c6351766e3df3
Mapet13 <jakubsordyl1@gmail.com>
2024-10-28 11:14:25 +0100

    fix first set

--- app/src/main/kotlin/org/exeval/parser/utilities/FirstSetCreator.kt
+++ app/src/main/kotlin/org/exeval/parser/utilities/FirstSetCreator.kt
@@ -10,18 +10,17 @@ fun <S> createFirstSet(grammar: Grammar<S>, nullable: Set<S>): Map<S, List<S>> {
     ).mapValues { it.value.distinct() }
 }
 
-private fun <S> crateMapWithOnlyKeySymbol(grammar: Grammar<S>): Map<S, MutableList<S>> {
-    return grammar.productions.map { it.left }.associateWith { mutableListOf(it) }
+private fun <S> crateMapWithOnlyKeySymbol(grammar: Grammar<S>): Map<S, MutableSet<S>> {
+    return grammar.productions.map { it.left }.associateWith { mutableSetOf(it) }
 }
 
 private fun <S> addPossibleStates(
-    first: Map<S, MutableList<S>>, grammar: Grammar<S>, nullable: Set<S>
-): Map<S, MutableList<S>> {
+    first: Map<S, MutableSet<S>>, grammar: Grammar<S>, nullable: Set<S>
+): Map<S, MutableSet<S>> {
     first.forEach { (symbol, firstForSymbol) ->
         grammar.productions.filter { it.left == symbol && it.right.isNotEmpty() }.forEach { productions ->
             for (currentSymbol in productions.right) {
-                if (!firstForSymbol.contains(currentSymbol) )
-                    firstForSymbol.add(currentSymbol)
+                firstForSymbol.add(currentSymbol)
                 if (!nullable.contains(currentSymbol)) break
             }
         }
@@ -29,12 +28,12 @@ private fun <S> addPossibleStates(
     return first
 }
 
-private fun <S> transitiveClosure(first: Map<S, MutableList<S>>): Map<S, MutableList<S>> {
+private fun <S> transitiveClosure(first: Map<S, MutableSet<S>>): Map<S, MutableSet<S>> {
     repeat(first.size) {
-        first.forEach { (_, list) ->
-            list.addAll(list.fold(mutableListOf()) { newList, symbol ->
-                newList.addAll(first[symbol].orEmpty())
-                newList
+        first.forEach { (_, set) ->
+            set.addAll(set.fold(mutableSetOf()) { newSet, symbol ->
+                newSet.addAll(first[symbol].orEmpty())
+                newSet
             })
         }
     }
