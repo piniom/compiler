285a911f65f51d0898748cf3516dba2e161cebc2
Jakub Siuta <jakub.siuta@mim.ai>
2024-11-04 21:39:21 +0100

    Add ConstChecker class

--- app/src/main/kotlin/org/exeval/ast/ConstChecker.kt
+++ app/src/main/kotlin/org/exeval/ast/ConstChecker.kt
@@ -2,11 +2,27 @@ package org.exeval.ast
 
 import org.exeval.utilities.interfaces.Diagnostics
 import org.exeval.ast.NameResolution
-
+import org.exeval.input.interfaces.Location
+import ConstantDeclaration
 
 class ConstChecker{
-    fun check(NameResolution: NameResolution): Diagnostics?{
-       
-        return null
+    fun check(nameResolution: NameResolution, astInfo: AstInfo): List<Diagnostics>{
+        
+        val errors: MutableList<Diagnostics> = mutableListOf()
+        nameResolution.assignmentToDecl.forEach{ (assignemt, declaration) ->
+            when(declaration){
+                is ConstantDeclaration -> {
+                    errors.add( object: Diagnostics {
+                        override val message: String = "An illegall assignement to a constant value"
+                        override val startLocation: Location = astInfo.locations[declaration]!!.start
+                        override val stopLocation: Location = astInfo.locations[assignemt]!!.start 
+                    }) 
+                }
+                else -> {
+
+                }
+            }
+        }
+        return errors.toList() 
     }  
 }
---
+++ app/src/test/kotlin/org/exeval/ast/ConstCheckerTest.kt
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test
+import org.exeval.ast.ConstChecker
+import org.exeval.ast.NameResolution
+import org.exeval.ast.IntType
+import org.exeval.ast.AstInfo
+import org.exeval.utilities.LocationRange
+import org.exeval.input.interfaces.Location
+
+class ConstCheckerTest{
+    @Test
+    fun assignementToConst(){
+        //given
+        val constChecker = ConstChecker()
+        val ass1 = Assignment("a", IntLiteral(1)) 
+        val dec1 = ConstantDeclaration("a", IntType, IntLiteral(2))
+        val nameResolution = NameResolution(mapOf(), mapOf(), mapOf(), mapOf(), mapOf(Pair(ass1, dec1)))
+        val loc1: Location = object : Location{ override var line = 1; override var idx = 1} 
+        val loc2: Location = object : Location{ override var line = 2; override var idx = 1} 
+        val loc3: Location = object : Location{ override var line = 3; override var idx = 1} 
+        val loc4: Location = object : Location{ override var line = 4; override var idx = 1} 
+        val astInfo = AstInfo(dec1, mapOf(Pair(ass1, LocationRange(loc1, loc2)), Pair(dec1, LocationRange(loc3, loc4))))
+
+        //when
+        val errors = constChecker.check(nameResolution, astInfo)
+
+        //then
+
+        assert(errors.isNotEmpty())
+    }
+    @Test
+    fun assignementToMut(){
+        //given
+        val constChecker = ConstChecker()
+        val ass1 = Assignment("a", IntLiteral(1)) 
+        val dec1 = MutableVariableDeclaration("a", IntType)
+        val nameResolution = NameResolution(mapOf(), mapOf(), mapOf(), mapOf(), mapOf(Pair(ass1, dec1)))
+        val loc1: Location = object : Location{ override var line = 1; override var idx = 1} 
+        val loc2: Location = object : Location{ override var line = 2; override var idx = 1} 
+        val loc3: Location = object : Location{ override var line = 3; override var idx = 1} 
+        val loc4: Location = object : Location{ override var line = 4; override var idx = 1} 
+        val astInfo = AstInfo(dec1, mapOf(Pair(ass1, LocationRange(loc1, loc2)), Pair(dec1, LocationRange(loc3, loc4))))
+
+        //when
+        val errors = constChecker.check(nameResolution, astInfo)
+
+        //then
+
+        assert(errors.isEmpty())
+    }
+}
