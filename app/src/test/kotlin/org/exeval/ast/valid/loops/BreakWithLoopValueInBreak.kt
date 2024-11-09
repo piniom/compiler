34573bd5a1e5fe6bd3011ad46a883201520ea129
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2024-11-09 10:04:50 +0100

    added loops example ast

---
+++ app/src/test/kotlin/org/exeval/ast/valid/loops/BreakWithLoopValueInBreak.kt
@@ -0,0 +1,46 @@
+package org.exeval.ast.valid.loops
+
+import org.exeval.ast.*
+
+val LOOPS_BREAK_WITH_LOOP_VALUE_IN_BREAK = Program(
+    functions = listOf(
+        FunctionDeclaration(
+            name = "main",
+            parameters = listOf(),
+            returnType = IntType,
+            body = Block(
+                expressions = listOf(
+                    Loop(
+                        identifier = null,
+                        body = Block(
+                            expressions = listOf(
+                                Break(
+                                    identifier = null,
+                                    expression = BinaryOperation(
+                                        left = BinaryOperation(
+                                            left = IntLiteral(value = 7),
+                                            operator = BinaryOperator.PLUS,
+                                            right = IntLiteral(value = 19)
+                                        ),
+                                        operator = BinaryOperator.MULTIPLY,
+                                        right = Loop(
+                                            identifier = null,
+                                            body = Block(
+                                                expressions = listOf(
+                                                    Break(
+                                                        identifier = null,
+                                                        expression = IntLiteral(value = 2)
+                                                    )
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    )
+)
