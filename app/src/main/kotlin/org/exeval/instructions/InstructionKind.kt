3d1bf2f7be227e77d3972278d190fda8318377e5
PiJayson <73486294+PiJayson@users.noreply.github.com>
2024-11-23 10:43:42 +0100

    stage06/Implement InstructionSet (#110)
    * Created base for InstructionSet
    
    * Add patterns for most binary operations (without comparisons)
    
    * Add patterns for most other operations
    
    * Add partial pattern for assignment
    
    * Added WorkingRegisters and some proposition to instruction patterns
    
    * Add missing cases needing additional/specific registers
    
    * Fix tests broken by WorkingRegisters
    
    * Add conversion from boolean (0 / non-zero) to 0/1
    
    * Add patterns for comparison operations
    
    * Moved Call to UnaryOperatinType, Fixed typos
    
    * Fixed typo with Assignment
    
    * Fix some of the errors
    
    * Make destRegister nullable, rename REGISTER_SIZE to REGISTER_COUNT
    
    * Adjust instruction coverer to new Instr match result api
    
    ---------
    
    Co-authored-by: Agata Margas <agata.margas@student.uj.edu.pl>
    Co-authored-by: KacKoz <kacpro@onet.com.pl>

--- app/src/main/kotlin/org/exeval/instructions/InstructionKind.kt
+++ app/src/main/kotlin/org/exeval/instructions/InstructionKind.kt
@@ -3,8 +3,3 @@ package org.exeval.instructions
 enum class InstructionKind {
     VALUE, JUMP, EXEC
 }
-
-enum class OperationType {
-    ADD, SUBTRACT, MULTIPLY, DIVIDE, MODULO, AND, OR, XOR, GREATER, GREATER_EQUAL, EQUAL,
-    NOT, INCREMENT, DECREMENT, CALL, RETURN
-}
