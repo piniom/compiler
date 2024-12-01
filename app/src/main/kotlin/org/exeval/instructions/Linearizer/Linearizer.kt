095be55242f86dca721b3dfcf36f3acebc07313f
prolik123 <mattti2015@gmail.com>
2024-12-01 14:39:23 +0100

    make linearization linear

--- app/src/main/kotlin/org/exeval/instructions/Linearizer/Linearizer.kt
+++ app/src/main/kotlin/org/exeval/instructions/Linearizer/Linearizer.kt
@@ -8,43 +8,47 @@ import org.exeval.instructions.InstructionCovererInterface;
 class Linearizer(private val instructionCoverer : InstructionCovererInterface) {
 
     public fun createBasicBlocks(node : CFGNode?) : List<BasicBlock> {
-        return makeBasicBlocks(node).asReversed()
+        var result = mutableListOf<BasicBlock>()
+        makeBasicBlocks(node, result)
+        return result.asReversed()
     }
 
     // create reversed list of basic blocks (revesed due to time saving)
-    private fun makeBasicBlocks(node : CFGNode?) : MutableList<BasicBlock> {
+    private fun makeBasicBlocks(node : CFGNode?, result : MutableList<BasicBlock>) {
 
         if (node == null)
-            return mutableListOf()
+            return
 
         var basicContent = mutableListOf<Instruction>()
         for (tree in node.trees)
             basicContent.addAll(instructionCoverer.cover(tree))
 
-        if (node.branches == null || node.branches?.first == null)
-            return mutableListOf(BasicBlock(generateLabel(), basicContent, mutableListOf()))
+        if (node.branches == null || node.branches?.first == null) {
+            result.add(BasicBlock(generateLabel(), basicContent, mutableListOf()))
+            return
+        }
         
         if (node.branches?.second == null) {
-            var continuationList = makeBasicBlocks(node.branches?.first)
+            makeBasicBlocks(node.branches?.first, result)
             var successors = mutableListOf<BasicBlock>()
-            if (continuationList.lastOrNull() != null)
-                successors.add(continuationList.last())
-            continuationList.add(BasicBlock(generateLabel(), basicContent, successors))
-            return continuationList
+            if (result.lastOrNull() != null)
+                successors.add(result.last())
+            result.add(BasicBlock(generateLabel(), basicContent, successors))
+            return
         }
 
-        var firstContinuation = makeBasicBlocks(node.branches?.first)
-        var secondContinuation = makeBasicBlocks(node.branches?.second)
-
+        
         var successors = mutableListOf<BasicBlock>()
-        if (firstContinuation.lastOrNull() != null)
-            successors.add(firstContinuation.last())
-        if (secondContinuation.lastOrNull() != null)
-            successors.add(secondContinuation.last())
 
-        secondContinuation.addAll(firstContinuation)
-        secondContinuation.add(BasicBlock(generateLabel(), basicContent, successors))
-        return secondContinuation
+        makeBasicBlocks(node.branches?.second, result)
+        if (result.lastOrNull() != null)
+            successors.add(result.last())
+
+        makeBasicBlocks(node.branches?.first, result)
+        if (result.lastOrNull() != null)
+            successors.add(result.last())
+
+        result.add(BasicBlock(generateLabel(), basicContent, successors))
     }
 
     private fun generateLabel() : DataLabel {
