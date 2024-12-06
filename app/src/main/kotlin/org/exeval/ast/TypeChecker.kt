030158cad7cfed1e0d4d3708a30326853adad165
PiJayson <pijay888@gmail.com>
2024-12-06 12:56:41 +0100

    By default break return NopeType

--- app/src/main/kotlin/org/exeval/ast/TypeChecker.kt
+++ app/src/main/kotlin/org/exeval/ast/TypeChecker.kt
@@ -171,7 +171,7 @@ class TypeChecker(private val astInfo: AstInfo, private val nameResolutionResult
 
     private fun getBreakType(breakEl: Break) {
         val identifier = breakEl.identifier
-        val expressionType = breakEl.expression?.let { innerParse(it) }
+        val expressionType = breakEl.expression?.let { innerParse(it) } ?: NopeType
 
         // Check if break without label
         if (identifier == null) {
