74e5c0b22ff44541ec7f9dd6fdca6b2ce16a4387
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2024-12-06 18:28:15 +0100

    removed redundant comments

--- app/src/main/kotlin/org/exeval/cfg/TreeKind.kt
+++ app/src/main/kotlin/org/exeval/cfg/TreeKind.kt
@@ -1,15 +1,12 @@
 package org.exeval.cfg
 
-// Define the TreeKind interface
 interface TreeKind
 
-// Implement TreeKind as singleton objects for each type
 object ConstantTreeKind : TreeKind
 object MemoryTreeKind : TreeKind
 object RegisterTreeKind : TreeKind
 object AssignmentTreeKind : TreeKind
 
-// Binary operation kinds
 object BinaryAddTreeKind : TreeKind
 object BinarySubtractTreeKind : TreeKind
 object BinaryMultiplyTreeKind : TreeKind
@@ -22,10 +19,8 @@ object BinaryEqualTreeKind : TreeKind
 object BinaryLessTreeKind : TreeKind
 object BinaryLessEqualTreeKind : TreeKind
 
-// Unary operation kinds
 object UnaryNotTreeKind : TreeKind
 object UnaryMinusTreeKind : TreeKind
 
-// Other kinds
 object CallTreeKind : TreeKind
 object ReturnTreeKind : TreeKind
