a594995ab035c5fb37fd7b9e2a47bae0d2d4fe34
Jakub Siuta <jakub.siuta@mim.ai>
2024-12-09 15:07:17 +0100

    Add implementation

---
+++ app/src/main/kotlin/org/exeval/instructions/LivenessCheckerImpl.kt
@@ -0,0 +1,75 @@
+package org.exeval.instructions
+
+import org.exeval.cfg.Register
+import org.exeval.instructions.Instruction
+import org.exeval.instructions.interfaces.LivenessChecker
+import org.exeval.instructions.interfaces.LivenessResult
+import org.exeval.instructions.linearizer.BasicBlock
+
+class LivenessCheckerImpl: LivenessChecker {
+    override fun check(basicBlocks: List<BasicBlock>): LivenessResult {
+        var liveIn: MutableMap<Instruction, Set<Register>> = mutableMapOf()
+        var liveOut: MutableMap<Instruction, Set<Register>> = mutableMapOf()
+
+        val use: MutableMap<Instruction, Set<Register>> = mutableMapOf()
+        val def: MutableMap<Instruction, Set<Register>> = mutableMapOf()
+        var copyGraph: MutableMap<Register, MutableSet<Register>> = mutableMapOf()
+        var inferenceGraph: MutableMap<Register, MutableSet<Register>> = mutableMapOf()
+        for (bb in basicBlocks){
+            for( instr in bb.instructions){
+                use[instr] = instr.usedRegisters().toSet()
+                def[instr] = instr.definedRegisters().toSet()
+                liveOut[instr] = instr.definedRegisters().toSet()
+                liveIn[instr] = instr.usedRegisters().toSet()
+
+                for(r in (instr.usedRegisters() union instr.definedRegisters())){
+                    if(!copyGraph.containsKey(r)) copyGraph[r] = mutableSetOf()
+                    if(!inferenceGraph.containsKey(r)) inferenceGraph[r] = mutableSetOf()
+                }
+
+                if (instr.isCopy()){
+                    copyGraph[instr.definedRegisters().first()]!!.add(instr.usedRegisters().first())
+                    inferenceGraph[instr.usedRegisters().first()]!!.add(instr.definedRegisters().first())
+                }
+            }
+        }
+        var fixedPoint = false
+        while(fixedPoint == false) {
+            fixedPoint = true
+            for (i in 0..<basicBlocks.size) {
+                val bb = basicBlocks[i]
+                for (j in 0..<bb.instructions.size) {
+                    val next: List<Instruction> =
+                        if (i == basicBlocks.size - 1 && j == bb.instructions.size - 1) listOf()
+                        else if (j == bb.instructions.size - 1) bb.successors.map { b -> b.instructions.first() }
+                            .toList()
+                        else listOf(bb.instructions[j + 1])
+                    val instruction = bb.instructions[j]
+
+                    val oldLiveIn = liveIn[instruction]!!
+                    val oldLiveOut = liveOut[instruction]!!
+
+                    val newLiveIn = use[instruction]!! union (oldLiveOut subtract def[instruction]!!)
+                    var newLiveOut = def[instruction]!!
+                    for (nextInstruction in next) {
+                        newLiveOut = newLiveOut union liveIn[nextInstruction]!!
+                    }
+
+                    if ( oldLiveIn != newLiveIn || oldLiveOut != newLiveOut)
+                        fixedPoint = false
+                }
+            }
+        }
+
+        for((instruction, registers) in def) {
+            for (register in registers) {
+                inferenceGraph[register]!!.addAll(liveOut[instruction]!!)
+            }
+        }
+        for((register, set) in inferenceGraph) {
+            inferenceGraph[register]!!.removeAll(copyGraph[register]!!)
+        }
+        return LivenessResult(inferenceGraph, copyGraph)
+    }
+
+}
