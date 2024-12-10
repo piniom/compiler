efa8d77545ba831ceaeed04edbe8a2513a76d35d
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2024-12-10 15:10:42 +0100

    removed auto-generated main::App

--- app/src/main/kotlin/org/exeval/App.kt
+++ app/src/main/kotlin/org/exeval/App.kt
@@ -22,21 +22,10 @@ import org.exeval.utilities.TokenCategories
 
 private val logger = KotlinLogging.logger {}
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello World!"
-        }
-
-    fun constructorsUsage() {
-        val regexParser = RegexParserImpl()
-    }
-}
-
 fun buildLexer(): Lexer {
     val tokens = TokenCategories.values()
     val regexParser = RegexParserImpl()
-    val dfas = tokens.associateBy({ regexToDfa(regexParser, it.regex) } , { it })
+    val dfas = tokens.associateBy({ regexToDfa(regexParser, it.regex) }, { it })
     val lexer = MultipleTokensLexer(dfas)
     return lexer
 }
@@ -47,7 +36,7 @@ private fun regexToDfa(regexParser: RegexParser, regexString: String): DFA<*> {
 
     val nfa = nfaParser.parse(regex)
     val dfaParser = DFAParserImpl<Any>()
-    val dfa = dfaParser.parse(nfa) as DFA<Any>
+    @Suppress("UNCHECKED_CAST") val dfa = dfaParser.parse(nfa) as DFA<Any>
     val minimizer = DFAmin<Any>()
     val dfaMin = minimizer.minimize(dfa)
     return dfaMin
@@ -57,20 +46,20 @@ fun buildInput(fileName: String): Input {
     try {
         return CommentCutter(FileInput(fileName))
     } catch (e: FileNotFoundException) {
-        logger.error{"Input file `${fileName}' does not exist"}
+        logger.error { "Input file `${fileName}' does not exist" }
         exitProcess(1)
     }
 }
 
 fun main(args: Array<String>) {
     if (args.size == 0) {
-        logger.error{"Input file not provided. Use `./gradlew run --args=\"<file name>\"'"}
+        logger.error { "Input file not provided. Use `./gradlew run --args=\"<file name>\"'" }
         exitProcess(1)
     }
     val sourceCode = buildInput(args[0])
     val lexer = buildLexer()
     val lexerOutput = lexer.run(sourceCode)
     for (diagnostic in lexerOutput.diagnostics) {
-        logger.warn{"[Lexer diagnostic] ${diagnostic.message}"}
+        logger.warn { "[Lexer diagnostic] ${diagnostic.message}" }
     }
 }
