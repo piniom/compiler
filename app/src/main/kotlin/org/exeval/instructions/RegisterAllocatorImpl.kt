818570d4df3d652150bca52ba773b22d45ee31b9
prolik123 <mattti2015@gmail.com>
2024-12-14 15:20:10 +0100

    added copy merging for register allocator

--- app/src/main/kotlin/org/exeval/instructions/RegisterAllocatorImpl.kt
+++ app/src/main/kotlin/org/exeval/instructions/RegisterAllocatorImpl.kt
@@ -2,25 +2,49 @@ package org.exeval.instructions
 
 import org.exeval.cfg.PhysicalRegister
 import org.exeval.cfg.Register
+import org.exeval.cfg.VirtualRegister
 import org.exeval.instructions.interfaces.AllocationResult
 import org.exeval.instructions.interfaces.LivenessResult
 import org.exeval.instructions.interfaces.MutableRegisterGraph
+import org.exeval.instructions.interfaces.RegisterGraph
 import org.exeval.instructions.interfaces.RegisterAllocator
 
 class RegisterAllocatorImpl : RegisterAllocator {
+
     override fun allocate(
         livenessResult: LivenessResult,
         domain: Set<Register>,
         range: Set<PhysicalRegister>
     ): AllocationResult {
-        val mapping: MutableMap<Register, PhysicalRegister> = mutableMapOf()
-        val spills: MutableSet<Register> = mutableSetOf()
 
-        val coalescence : MutableMap<Register, Set<Register>> = domain.map { it to setOf(it) }.toMap().toMutableMap()
+        val mapping: MutableMap<Register, PhysicalRegister> = mutableMapOf()
+        val spills: MutableSet<VirtualRegister> = mutableSetOf()
         val graph = livenessResult.interference
+        val copyGraph = livenessResult.copy
+        /*val possibleRegisters = domain.toList()
+        val graphsDomain = domain.toMutableSet()
+        val numOfAvailableColors = range.size
+
+        for (register in possibleRegisters) {
+            if (!graphsDomain.contains(register))
+                continue
+
+            if (!copyGraph.containsKey(register))
+                continue
+            
+            for (copyReg in copyGraph[register]!!) {
+                if (!checkIfMergeIsSafe(graph, register, copyReg, numOfAvailableColors))
+                    continue
+                mergeToVertex(graph, copyGraph, register, copyReg)
+                graphsDomain.remove(copyReg)
+            }
+        }*/
 
         //...
 
+        
+
+        val coalescence : Map<Register, Set<Register>> = CoalescenceGraphCreator().createCoalescenceGraph(graph, copyGraph, domain, range.size)
         val visited: MutableSet<Register> = domain.filter { it is PhysicalRegister }.toMutableSet()
         val available: MutableSet<Register> = domain.subtract(visited).toMutableSet()
         val order: MutableList<Register> = visited.toMutableList()
@@ -37,6 +61,10 @@ class RegisterAllocatorImpl : RegisterAllocator {
 
         val colored: MutableSet<Register> = mutableSetOf()
         for (vertex in order) {
+
+            if (mapping.containsKey(vertex))
+                continue
+
             val physicalRegister = coalescence[vertex]!!.firstOrNull { it is PhysicalRegister } as PhysicalRegister?
             if (physicalRegister !== null ) {
                 colored.add(vertex)
@@ -46,12 +74,13 @@ class RegisterAllocatorImpl : RegisterAllocator {
             } else {
                 val availableRegisters = range subtract graph[vertex]!!.filter { colored.contains(it) }.map { mapping[it]!! }
                 if (availableRegisters.isEmpty() ) {
-                    coalescence[vertex]!!.forEach { spills.add(it) }
+                    coalescence[vertex]!!.forEach { spills.add(it as VirtualRegister) }
                 } else {
                     colored.add(vertex)
                     coalescence[vertex]!!.forEach { mapping.put(it, availableRegisters.first()) }
                 }
             }
+
         }
 
         return AllocationResult(mapping, spills)
