1744eef5bce4bac3cc7d9bd89c10c242c183ab47
prolik123 <mattti2015@gmail.com>
2024-12-14 15:24:58 +0100

    merge

--- app/src/main/kotlin/org/exeval/instructions/RegisterAllocatorImpl.kt
+++ app/src/main/kotlin/org/exeval/instructions/RegisterAllocatorImpl.kt
@@ -2,7 +2,6 @@ package org.exeval.instructions
 
 import org.exeval.cfg.PhysicalRegister
 import org.exeval.cfg.Register
-<<<<<<< HEAD
 import org.exeval.cfg.VirtualRegister
 import org.exeval.instructions.interfaces.AllocationResult
 import org.exeval.instructions.interfaces.LivenessResult
@@ -12,59 +11,17 @@ import org.exeval.instructions.interfaces.RegisterAllocator
 
 class RegisterAllocatorImpl : RegisterAllocator {
 
-=======
-import org.exeval.instructions.interfaces.AllocationResult
-import org.exeval.instructions.interfaces.LivenessResult
-import org.exeval.instructions.interfaces.MutableRegisterGraph
-import org.exeval.instructions.interfaces.RegisterAllocator
-
-class RegisterAllocatorImpl : RegisterAllocator {
->>>>>>> master
     override fun allocate(
         livenessResult: LivenessResult,
         domain: Set<Register>,
         range: Set<PhysicalRegister>
     ): AllocationResult {
-<<<<<<< HEAD
 
         val mapping: MutableMap<Register, PhysicalRegister> = mutableMapOf()
         val spills: MutableSet<VirtualRegister> = mutableSetOf()
         val graph = livenessResult.interference
         val copyGraph = livenessResult.copy
-        /*val possibleRegisters = domain.toList()
-        val graphsDomain = domain.toMutableSet()
-        val numOfAvailableColors = range.size
-
-        for (register in possibleRegisters) {
-            if (!graphsDomain.contains(register))
-                continue
-
-            if (!copyGraph.containsKey(register))
-                continue
-            
-            for (copyReg in copyGraph[register]!!) {
-                if (!checkIfMergeIsSafe(graph, register, copyReg, numOfAvailableColors))
-                    continue
-                mergeToVertex(graph, copyGraph, register, copyReg)
-                graphsDomain.remove(copyReg)
-            }
-        }*/
-
-        //...
-
-        
-
         val coalescence : Map<Register, Set<Register>> = CoalescenceGraphCreator().createCoalescenceGraph(graph, copyGraph, domain, range.size)
-=======
-        val mapping: MutableMap<Register, PhysicalRegister> = mutableMapOf()
-        val spills: MutableSet<Register> = mutableSetOf()
-
-        val coalescence : MutableMap<Register, Set<Register>> = domain.map { it to setOf(it) }.toMap().toMutableMap()
-        val graph = livenessResult.interference
-
-        //...
-
->>>>>>> master
         val visited: MutableSet<Register> = domain.filter { it is PhysicalRegister }.toMutableSet()
         val available: MutableSet<Register> = domain.subtract(visited).toMutableSet()
         val order: MutableList<Register> = visited.toMutableList()
@@ -81,13 +38,10 @@ class RegisterAllocatorImpl : RegisterAllocator {
 
         val colored: MutableSet<Register> = mutableSetOf()
         for (vertex in order) {
-<<<<<<< HEAD
 
             if (mapping.containsKey(vertex))
                 continue
 
-=======
->>>>>>> master
             val physicalRegister = coalescence[vertex]!!.firstOrNull { it is PhysicalRegister } as PhysicalRegister?
             if (physicalRegister !== null ) {
                 colored.add(vertex)
@@ -97,26 +51,14 @@ class RegisterAllocatorImpl : RegisterAllocator {
             } else {
                 val availableRegisters = range subtract graph[vertex]!!.filter { colored.contains(it) }.map { mapping[it]!! }
                 if (availableRegisters.isEmpty() ) {
-<<<<<<< HEAD
                     coalescence[vertex]!!.forEach { spills.add(it as VirtualRegister) }
-=======
-                    coalescence[vertex]!!.forEach { spills.add(it) }
->>>>>>> master
                 } else {
                     colored.add(vertex)
                     coalescence[vertex]!!.forEach { mapping.put(it, availableRegisters.first()) }
                 }
             }
-<<<<<<< HEAD
-
-=======
->>>>>>> master
         }
 
         return AllocationResult(mapping, spills)
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> master
