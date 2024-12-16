8c250a30595548ca0e5fd85b5a4abf367b175127
Mateusz Hurkała <58479434+prolik123@users.noreply.github.com>
2024-12-16 23:20:44 +0100

    Stage10 consider copy graph to register allocation (#187)
    * Add register allocator implementation
    
    * register allocation test - 1st draft
    
    * 1.1 version - changed comparison in simulation
    
    * reprepare for copy constaraints implementation
    
    * Adjust UTs
    
    * Fix tests
    
    * test update - instructions + 2nd case for noSpill
    
    * added copy merging for register allocator
    
    * merge
    
    * Adjust using RegisterGraph
    
    ---------
    
    Co-authored-by: Dominik Chmura <dompawchmura@gmail.com>
    Co-authored-by: Mikolaj Jachowicz <mikolaj4jach@gmail.com>
    Co-authored-by: Szymon Wojtulewicz <58044978+piniom@users.noreply.github.com>

--- app/src/main/kotlin/org/exeval/instructions/RegisterAllocatorImpl.kt
+++ app/src/main/kotlin/org/exeval/instructions/RegisterAllocatorImpl.kt
@@ -2,25 +2,26 @@ package org.exeval.instructions
 
 import org.exeval.cfg.PhysicalRegister
 import org.exeval.cfg.Register
+import org.exeval.cfg.VirtualRegister
 import org.exeval.instructions.interfaces.AllocationResult
 import org.exeval.instructions.interfaces.LivenessResult
 import org.exeval.instructions.interfaces.MutableRegisterGraph
+import org.exeval.instructions.interfaces.RegisterGraph
 import org.exeval.instructions.interfaces.RegisterAllocator
 
 class RegisterAllocatorImpl : RegisterAllocator {
+
     override fun allocate(
         livenessResult: LivenessResult,
         domain: Set<Register>,
         range: Set<PhysicalRegister>
     ): AllocationResult {
+
         val mapping: MutableMap<Register, PhysicalRegister> = mutableMapOf()
-        val spills: MutableSet<Register> = mutableSetOf()
-
-        val coalescence : MutableMap<Register, Set<Register>> = domain.map { it to setOf(it) }.toMap().toMutableMap()
+        val spills: MutableSet<VirtualRegister> = mutableSetOf()
         val graph = livenessResult.interference
-
-        //...
-
+        val copyGraph = livenessResult.copy
+        val coalescence: RegisterGraph = CoalescenceGraphCreator().createCoalescenceGraph(graph, copyGraph, domain, range.size)
         val visited: MutableSet<Register> = domain.filter { it is PhysicalRegister }.toMutableSet()
         val available: MutableSet<Register> = domain.subtract(visited).toMutableSet()
         val order: MutableList<Register> = visited.toMutableList()
@@ -37,6 +38,10 @@ class RegisterAllocatorImpl : RegisterAllocator {
 
         val colored: MutableSet<Register> = mutableSetOf()
         for (vertex in order) {
+
+            if (mapping.containsKey(vertex))
+                continue
+
             val physicalRegister = coalescence[vertex]!!.firstOrNull { it is PhysicalRegister } as PhysicalRegister?
             if (physicalRegister !== null ) {
                 colored.add(vertex)
@@ -46,7 +51,7 @@ class RegisterAllocatorImpl : RegisterAllocator {
             } else {
                 val availableRegisters = range subtract graph[vertex]!!.filter { colored.contains(it) }.map { mapping[it]!! }
                 if (availableRegisters.isEmpty() ) {
-                    coalescence[vertex]!!.forEach { spills.add(it) }
+                    coalescence[vertex]!!.forEach { spills.add(it as VirtualRegister) }
                 } else {
                     colored.add(vertex)
                     coalescence[vertex]!!.forEach { mapping.put(it, availableRegisters.first()) }
