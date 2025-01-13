69bc709f9bbc3557c8bd8901e59d54110bb2a3bd
KacKoz <kacpro@onet.com.pl>
2025-01-13 12:16:45 +0100

    Link stdlib .so file

--- app/src/main/kotlin/org/exeval/App.kt
+++ app/src/main/kotlin/org/exeval/App.kt
@@ -38,6 +38,8 @@ import org.exeval.utilities.LexerUtils
 import org.exeval.utilities.interfaces.OperationResult
 import java.io.File
 import org.exeval.parser.grammar.PrecompiledParserFactory
+import java.nio.file.Paths
+import java.io.IOException
 
 private val logger = KotlinLogging.logger {}
 
@@ -52,6 +54,16 @@ fun main(args: Array<String>) {
 
     val linkedLibraries = mutableListOf<String>()
     val linkedLibrariesDirs = mutableListOf<String>()
+    // Add stdlib .so files
+    val cwd = Paths.get("").toAbsolutePath().toString()
+    // For now assumes that cwd = .../compiler/app and includes from .../compiler/stdlib
+    val stdlibDir = File(cwd.replaceAfterLast("/", "stdlib"))
+    val stdlibs = stdlibDir.walk().filter { it.name.endsWith((".so")) }
+    linkedLibrariesDirs.add(stdlibDir.absolutePath)
+    for(file in stdlibs) {
+        linkedLibraries.add(file.name)
+    }
+
     var i=1;
     while (i<args.size-1) {
         if(args[i] == "-l") {
@@ -81,12 +93,6 @@ fun main(args: Array<String>) {
     val nameResolutionOutput = NameResolutionGenerator(astInfo).parse()
     for (diagnostic in nameResolutionOutput.diagnostics) {
         logger.warn { "[NameResolution diagnostic] ${diagnostic.message}" }
-    }
-
-    val constChecker = ConstChecker()
-    val constErrors = constChecker.check(nameResolutionOutput.result, astInfo)
-    for (diagnostic in constErrors) {
-        logger.warn { "[ConstChecker diagnostic] ${diagnostic.message}" }
     }
 
     val typeCheckerOutput = TypeChecker(astInfo, nameResolutionOutput.result).parse()
