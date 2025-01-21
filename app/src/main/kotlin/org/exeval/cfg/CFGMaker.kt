90b4a10ff940e005e1f82ca5a7e06e637fe8f54a
Jakub Siuta <jakub.siuta@mim.ai>
2025-01-21 08:51:15 +0100

    Modify CFGMaker

--- app/src/main/kotlin/org/exeval/cfg/CFGMaker.kt
+++ app/src/main/kotlin/org/exeval/cfg/CFGMaker.kt
@@ -1,6 +1,8 @@
 package org.exeval.cfg
 
 import org.exeval.cfg.interfaces.CFGNode
+import org.exeval.cfg.MemoryNewTree
+import org.exeval.cfg.MemoryDelTree
 import org.exeval.cfg.AssignmentTree as CFGAssignment
 import org.exeval.cfg.BinaryOperationTree as BinaryOp
 import org.exeval.cfg.BinaryTreeOperationType as BinaryOpType
@@ -52,9 +54,9 @@ class CFGMaker(
             is UnaryOperation -> walkUnaryOperation(expr, then)
             is VariableDeclarationBase -> walkVariableDeclarationBase(expr, then)
             is VariableReference -> walkVariableReference(expr, then)
-            is ArrayAccess -> TODO()
-            is MemoryDel -> TODO()
-            is MemoryNew -> TODO()
+            is ArrayAccess -> walkArrayAccess(expr, then)
+            is MemoryDel -> walkMemoryDel(expr, then)
+            is MemoryNew -> walkMemoryNew(expr, then)
             is StructFieldAccess -> TODO("scheduled in task Structs #1")
             is ConstructorDeclaration -> TODO("scheduled in task Structs #1")
             is HereReference -> TODO("scheduled in task Structs #1")
@@ -78,6 +80,7 @@ class CFGMaker(
         )
         return WalkResult(value.top, null)
     }
+
 
     private fun walkBinaryOperation(bin: BinaryOperation, then: CFGNode): WalkResult {
         if (bin.operator == BinaryOperator.AND || bin.operator == BinaryOperator.OR) {
@@ -210,6 +213,7 @@ class CFGMaker(
             val expression = when (a) {
                 is NamedArgument -> a.expression
                 is PositionalArgument -> a.expression
+
             }
             val curUsage = varUsage[expression]!!
 
@@ -266,7 +270,55 @@ class CFGMaker(
             is NothingLiteral -> TODO("scheduled in task Structs #1")
         }
     }
+    private fun walkArrayAccess(acc: ArrayAccess, then: CFGNode): WalkResult{
+        val leftUsage = varUsage[acc.array]!!
+        val rightUsage = varUsage[acc.index]!!
+        val typeConst: Long = when(typeMap[acc.array]){
+            is ArrayType -> 8
+            is BoolType -> 1
+            is IntType -> 8
+            is StructType -> TODO()
+            else -> 8
+        }
+        if (leftUsage.conflicts(rightUsage)) {
+            val r = walkExpr(acc.index, then)
+            r.tree = BinaryOp(r.tree!!, NumericalConstantTree(typeConst), BinaryTreeOperationType.MULTIPLY)
+            val node = Node(r.top)
+            val l = walkExpr(acc.array, node)
+            val reg = newVirtualRegister()
+            node.trees = listOf(CFGAssignment(reg, l.tree!!))
+            return WalkResult(
+                l.top,
+                MemoryTree(BinaryOp(reg, r.tree!!, BinaryTreeOperationType.ADD))
+            )
+        } else {
+            val r = walkExpr(acc.index, then)
+            r.tree = BinaryOp(r.tree!!, NumericalConstantTree(typeConst), BinaryTreeOperationType.MULTIPLY)
+            val l = walkExpr(acc.array, r.top)
+            return WalkResult(
+                l.top,
+                MemoryTree(BinaryOp(l.tree!!, r.tree!!, BinaryTreeOperationType.ADD))
+            )
+        }
+    }
 
+    private fun walkMemoryNew(memNew: MemoryNew, then: CFGNode): WalkResult{
+        val inner = walkExpr(memNew.constructorArguments.first().expression, then)
+        val typeConst: Long = when(memNew.type){
+            is ArrayType -> 8
+            is BoolType -> 1
+            is IntType -> 8
+            is StructType -> TODO()
+            else -> 8
+        }
+        inner.tree = BinaryOp(inner.tree!!, NumericalConstantTree(typeConst), BinaryTreeOperationType.MULTIPLY)
+        return WalkResult(inner.top, MemoryNewTree(inner.tree!!))
+    }
+
+    private fun walkMemoryDel(memDel: MemoryDel, then: CFGNode): WalkResult{
+        val inner = walkExpr(memDel.pointer, then)
+        return WalkResult(inner.top, MemoryDelTree(inner.tree!!))
+    }
     private fun walkUnaryOperation(unaryOperation: UnaryOperation, then: CFGNode): WalkResult {
         if (unaryOperation.operator == UnaryOperator.NOT) {
             return walkLogicalExpressionShortCircuit(unaryOperation, then)
